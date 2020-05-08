037be4165ae7c3931795ae593a8c07e0db9245c2
Rezvan Farahibozorg <rezvanh@fmrib.ox.ac.uk>
2020-05-08 04:39:39 +0100

    update

--- semnet4semloc/.ipynb_checkpoints/Steppre5_MNE_eve_LD-checkpoint.py
+++ semnet4semloc/.ipynb_checkpoints/Steppre5_MNE_eve_LD-checkpoint.py
@@ -43,7 +43,7 @@ subject_inds = []#
 for ss in sys.argv[1:]:
     subject_inds.append( int( ss ) )
 
-subject_inds=[0]#,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]#[16,17,18]#
+subject_inds=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]#[16,17,18]#
 print ("subject_inds:")
 print (subject_inds)
 print ("No rejection")
