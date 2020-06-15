cdeaee72cb2101fca120e60004089d82787e7934
Rezvan Farahibozorg <rezvanh@fmrib.ox.ac.uk>
2020-06-15 22:00:09 +0100

    update

--- semnet4semloc/wrappers/sbatch_apply_inverse_evoked.sh
+++ semnet4semloc/wrappers/sbatch_apply_inverse_evoked.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbatch --job-name=Analysis1 --array 0-19 -l --time=24:00:00 --mem=32G -o ./qsub_out/wrapper_apply_inverse_evoked.out  /imaging/rf02/semnet_git/SemNet/semnet4semloc/wrappers/wrapper_batch.sh '/imaging/rf02/semnet_git/SemNet/semnet4semloc/Step6_1_MNE_Applyinverse_evoked.py'
+sbatch --job-name=Analysis1 --array 0-19 -l --time=24:00:00 --mem=32G -o ./qsub_out/wrapper_apply_inverse_evoked.out  /imaging/rf02/semnet_git/SemNet/semnet4semloc/wrappers/wrapper_sbatch.sh '/imaging/rf02/semnet_git/SemNet/semnet4semloc/Step6_1_MNE_Applyinverse_evoked.py'
