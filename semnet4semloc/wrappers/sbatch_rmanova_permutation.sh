2fda37a8234b10da23b866b874f37d79552fa412
Rezvan Farahibozorg <rezvanh@fmrib.ox.ac.uk>
2020-07-17 15:04:38 +0100

    update

---
+++ semnet4semloc/wrappers/sbatch_rmanova_permutation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbatch --job-name=Permutation2 --array 0-6 --time=24:00:00 --mincpus=4 --mem-per-cpu=8G -o /imaging/rf02/semnet_git_queue/wrapper_rmanova_permutation.out -e /imaging/rf02/semnet_git_queue/wrapper_rmanova_permutation.err /imaging/rf02/semnet_git/SemNet/semnet4semloc/wrappers/wrapper_sbatch.sh '/imaging/rf02/semnet_git/SemNet/semnet4semloc/avg_and_stat/Step3_2_MNE_Evoked_Cluster_Permutation_rmanova.py' 
