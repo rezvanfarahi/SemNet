3300a216c797f22817541891ab0f044a48a0643b
Rezvan Farahibozorg <rezvanh@fmrib.ox.ac.uk>
2020-08-12 19:54:40 +0100

    update

---
+++ semnet4semloc/Step7_4_DCM_ROI_GLMs.m
@@ -0,0 +1,34 @@
+
+exp1  = load('/imaging/rf02/Semnet/semnet4semloc/SemLoc_SD.mat');
+exp2a = load('/imaging/rf02/Semnet/semnet4semloc/SemNet_SD.mat');
+exp2b = load('/imaging/rf02/Semnet/semnet4semloc/SemNet_LD.mat');
+
+Nr = size(exp1.Datafm_wins,1)
+Nt = size(exp1.Datafm_wins,2)
+
+Ns(1) = size(exp1.Datafm_wins,4);
+Ns(2) = size(exp2a.Datafm_wins,4)
+
+X = [ones(Ns(1),1) zeros(Ns(1),2)];
+X = [X; zeros(2*Ns(2),1) kron(eye(2),ones(Ns(2),1))]
+X = [X [zeros(Ns(1),Ns(2)); eye(Ns(2)); eye(Ns(2))]];
+figure,imagesc(X)
+
+Fcom = []; Pcom = []; Fdif = []; Pdif = [];
+for r = 1:Nr
+    for t = 1:Nt
+        y =     squeeze(exp1.Datafm_wins(r,t,:,:))'  * [1 -1]'; % contrast of conditions
+        y = [y; squeeze(exp2a.Datafm_wins(r,t,:,:))' * [1 -1]']; % contrast of conditions
+        y = [y; squeeze(exp2b.Datafm_wins(r,t,:,:))' * [1 -1]']; % contrast of conditions
+        %[Tval,Fcom(r,t),Pcom(r,t),df,R2,cR2,B,res,aR2,iR2,Bcov] = glm(y,X,[1 1 1 zeros(1,Ns(2))]');
+        [Tval,Fcom(r,t),Pcom(r,t),df,R2,cR2,B,res,aR2,iR2,Bcov] = glm(y,X,[1 1 1 ones(1,Ns(2))*2/Ns(2)]'); % http://www.sbirc.ed.ac.uk/cyril/download/Contrast_Weighting_Glascher_Gitelman_2008.pdf
+        [Tval,Fdif(r,t),Pdif(r,t),df,R2,cR2,B,res,aR2,iR2,Bcov] = glm(y,X,[detrend(eye(3),0) zeros(3,Ns(2))]');
+    end
+end
+
+alpha = 0.05 % uncorrected
+%alpha = alpha/(Nr*Nt) % extreme Bonferonni, ignoring that time windows correlated
+%alpha = alpha/Nr % compromise!
+
+figure,imagesc(Pcom),colorbar,xlabel('Twin'),ylabel('ROI'),set(gca,'XTick',[1:4],'YTick',[1:5]),colormap('gray'),caxis([0 alpha]),title('Common condition effect across exps')
+figure,imagesc(Pdif),colorbar,xlabel('Twin'),ylabel('ROI'),set(gca,'XTick',[1:4],'YTick',[1:5]),colormap('gray'),caxis([0 alpha]),title('Interaction between exp and condition')
