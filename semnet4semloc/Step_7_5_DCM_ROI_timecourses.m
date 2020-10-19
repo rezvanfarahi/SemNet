1965b53fbcaaa14f26849068e4260ff1352e1f7a
Rezvan Farahibozorg <rezvanh@fmrib.ox.ac.uk>
2020-10-19 18:08:04 +0100

    update

---
+++ semnet4semloc/Step_7_5_DCM_ROI_timecourses.m
@@ -0,0 +1,32 @@
+
+clear
+clc
+close all
+inpath='/Users/rezvanh/Documents/PhD/manuscripts/semloc_july2020/semnet4semloc/';
+filenames={'SemLoc_SD_100wins.mat','SemNet_SD_100wins.mat','SemNet_LD_100wins.mat'};
+X=zeros(5,100,2,53);
+for ii=1:3
+    inname=[inpath,filenames{ii}];
+    load(inname)
+    if ii==1
+        X(:,:,:,1:17)=Datafm_wins;
+    end
+    if ii==2
+        X(:,:,:,18:35)=Datafm_wins;
+    end
+    if ii==3
+        X(:,:,:,36:end)=Datafm_wins;
+    end
+    
+end
+Xm=mean(X,4);
+for ii=1:5
+    figure();
+    conmat=squeeze(Xm(ii,:,1));
+    absmat=squeeze(Xm(ii,:,2));
+    plot(-50:5:449,conmat,'r','LineWidth',5)
+    hold on   
+    plot(-50:5:449,absmat,'b','LineWidth',5)
+    set(gca,'FontSize',20)
+    legend('Concrete','Abstract')
+end
