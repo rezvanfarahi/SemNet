3c5d32e3426001a1129298395290dd24e2e02c5a
Rezvan Farahibozorg <rezvanh@fmrib.ox.ac.uk>
2020-10-21 18:25:05 +0100

    update

--- semnet4semloc/wrappers/wrapper_sbatch.sh
+++ semnet4semloc/wrappers/wrapper_sbatch.sh
@@ -7,5 +7,5 @@ echo $myvar
 # myvar: python script to execute
 # PBS_ARRAYID: subject index to be processed
 
-#/imaging/local/software/anaconda/2.4.1/2/bin/python2.7 ${myvar} $SLURM_ARRAY_TASK_ID
-/home/rf02/.conda/envs/profumo_flica/bin/python2.7 ${myvar} $SLURM_ARRAY_TASK_ID
+/imaging/local/software/anaconda/2.4.1/2/bin/python2.7 ${myvar} $SLURM_ARRAY_TASK_ID
+#/home/rf02/.conda/envs/profumo_flica/bin/python2.7 ${myvar} $SLURM_ARRAY_TASK_ID
